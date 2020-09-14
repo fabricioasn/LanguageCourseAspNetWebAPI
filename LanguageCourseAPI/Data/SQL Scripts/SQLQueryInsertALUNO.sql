32a41d548afe1ea9788929a007e411a8bac78449
Fabricio Almeida da Silva Nunes <32113848+fabricioasn@users.noreply.github.com>
2020-09-14 04:24:33 -0300

    SQL Insertion and Creation Scripts are located in Data/SQL Scripts directory

---
+++ LanguageCourseAPI/Data/SQL Scripts/SQLQueryInsertALUNO.sql
@@ -0,0 +1,20 @@
+USE [dbo.CursoDeIdiomas]
+GO
+
+INSERT INTO [dbo].[tbl_Aluno]
+           ([Nome_Completo]           
+           ,[Data_Nascimento]
+           ,[Telefone]           
+           ,[CPF]
+           ,[ClassRoomID]
+           ,[Matricula])
+     VALUES
+           ('Fabricio Almeida',
+            '1995/08/04',
+             '219999999',
+              '11122233344',
+              '1',
+              '1234567890');
+GO
+
+
---
+++ LanguageCourseAPI/Data/SQL Scripts/SQLQueryInsertTURMA.sql
@@ -0,0 +1,12 @@
+USE [dbo.CursoDeIdiomas]
+GO
+
+INSERT INTO [dbo].[tbl_Turma]
+           ([Idioma]
+           ,[Modulo])
+     VALUES
+           ('Ingles'
+           ,'Basico');
+GO
+
+
